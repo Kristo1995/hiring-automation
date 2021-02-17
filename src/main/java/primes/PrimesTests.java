47b6634a8ae1c3fb2c8340008213d9a54ff1e1c6
Kris <kristofer.radzinsh@gmail.com>
2021-02-17 21:51:21 +0000

    changes!

---
+++ src/main/java/primes/PrimesTests.java
@@ -0,0 +1,45 @@
+package primes;
+
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import util.TestUtil;
+import static io.restassured.RestAssured.*;
+
+public class PrimesTests {
+
+    @Test(dataProvider = "testStatusCodeEquals200", dataProviderClass = TestUtil.class)
+    public void testStatusCodeEquals200(String testcase, String value) throws Exception {
+
+        baseURI = TestUtil.readProperties("URL.properties", "URL_PRIMES");
+
+        String id = "/" + value;
+        System.out.println("Input -> " + value);
+
+        Response rs = given().get(id);
+        System.out.println("Response -> " + rs.getBody().asString());
+
+        int statusCode = rs.getStatusCode();
+        System.out.println("Status Code -> " + statusCode);
+
+        Assert.assertEquals(statusCode, 200 , "Testcase " + testcase + " has incorrect Status Code:");
+    }
+
+    @Test(dataProvider = "testStatusCodeEquals400", dataProviderClass = TestUtil.class)
+    public void testStatusCodeEquals400(String testcase, String value) throws Exception {
+
+        baseURI = TestUtil.readProperties("URL.properties", "URL_PRIMES");
+
+        String id = "/" + value;
+        System.out.println("Input -> " + value);
+
+        Response rs = given().get(id);
+        System.out.println("Response -> " + rs.getBody().asString());
+
+        int statusCode = rs.getStatusCode();
+        System.out.println("Status Code -> " + statusCode);
+
+        Assert.assertEquals(statusCode, 400 , "Testcase " + testcase + " has incorrect Status Code:");
+    }
+}
---
+++ src/main/java/util/TestUtil.java
@@ -0,0 +1,56 @@
+package util;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class TestUtil {
+
+    static String resourcePath ="src/main/resources/";
+    static String testData = resourcePath + "Testcases.xlsx";
+
+    public static String readProperties(String fileName, String key) throws Exception{
+
+        String fullFilename = resourcePath + fileName;
+        FileInputStream fis = new FileInputStream(fullFilename);
+        Properties props = new Properties();
+        props.load(fis);
+
+        return props.getProperty(key);
+    }
+
+    public static String[][] getExcelData(String fileName, String sheetName) throws Exception{
+
+        FileInputStream fis = new FileInputStream(fileName);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheet(sheetName);
+        DataFormatter formatter = new DataFormatter();
+
+        int numberOfColumns = sheet.getRow(1).getLastCellNum();
+        int numberOfRows = sheet.getLastRowNum();
+
+        String[][] excelData = new String[numberOfRows][numberOfColumns];
+
+        for(int i = 1; i <= numberOfRows; i++){
+            for (int j = 1; j <= numberOfColumns; j++){
+                excelData[i-1][j-1] = formatter.formatCellValue(sheet.getRow(i).getCell(j-1));
+            }
+        }
+
+        return excelData;
+    }
+
+    @DataProvider(name="testStatusCodeEquals200")
+    public static Object[][] testStatusCodeEquals200() throws Exception {
+        return getExcelData(testData,"testStatusCodeEquals200");
+    }
+
+    @DataProvider(name="testStatusCodeEquals400")
+    public static Object[][] testStatusCodeEquals400() throws Exception {
+        return getExcelData(testData,"testStatusCodeEquals400");
+    }
+}
